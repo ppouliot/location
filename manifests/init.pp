16530c4c32dfd5ff291d056629c333e89b67918d
Peter Pouliot <peter@pouliot.net>
2018-10-31 09:53:16 -0400

    Update init.pp
    Previous service was disabled and renamed to ipstack.com.   Additionally api endpoints changed.   Moving to new api model and urls.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,16 @@
-class location ( $url = "http://freegeoip.net/json/",
-                 $filename = "location.json" ) {
-
+class geolocation ( 
+  $baseurl = "http://api.ipstack.com/check/?access_key=",
+  $filename = "location.json"
+  $api_access_key
+  ) {
+  
+  # Request format for ipstack.com
+  # http://api.ipstack.com/134.201.250.155
+  #  ? access_key = YOUR_ACCESS_KEY
+  # 
+  # http://api.ipstack.com/check?access_key=<API_TOKEN>
+  # 
+  url = "${baseurl}${api_access_key}"
   fetchfact::fetch { '$filename':
     url => $url,
     factfile => $filename
